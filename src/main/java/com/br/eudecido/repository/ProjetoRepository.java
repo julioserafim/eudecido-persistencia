545e89ca834ce02e4b47f9d79179d1439f75c913
julioserafim <serafimjuliom583@gmail.com>
2016-11-28 16:49:46 -0300

    Alteração de JPA para Mongo.

--- src/main/java/com/br/eudecido/repository/ProjetoRepository.java
+++ src/main/java/com/br/eudecido/repository/ProjetoRepository.java
@@ -1,13 +1,13 @@
 package com.br.eudecido.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.br.eudecido.models.Projeto;
 import java.lang.String;
 import java.util.List;
 import com.br.eudecido.models.Usuario;
 
-public interface ProjetoRepository extends JpaRepository<Projeto, Integer>{
+public interface ProjetoRepository extends MongoRepository<Projeto, Integer>{
 	
 	List<Projeto> findByNome(String nome);
 	List<Projeto> findByUsuario(Usuario usuario);
--- src/main/java/com/br/eudecido/repository/UsuarioRepository.java
+++ src/main/java/com/br/eudecido/repository/UsuarioRepository.java
@@ -1,13 +1,14 @@
 package com.br.eudecido.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.br.eudecido.models.Usuario;
 import java.lang.String;
 import java.util.List;
 import java.lang.Integer;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+public interface UsuarioRepository extends MongoRepository<Usuario, Integer>{
 	List<Usuario> findByCpf(String cpf);
 	List<Usuario> findByEmail(String email);
 }
